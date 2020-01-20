4757ac605854868e403dfc2bfe1ca417a187e9c0
Explorer <43823966+exploreone@users.noreply.github.com>
2020-01-21 00:05:07 +0530

    Add files via upload
    River game

--- MainActivity.java
+++ MainActivity.java
@@ -1,35 +1,44 @@
-package com.example.sih2020;
+package com.example.swami;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
-    TextView tv1,tv2,tv3,tv4;
-
+public class MainActivity extends Activity {
+Button b1;
+TextView t1;
+TextView t2,t3,t4;
+Animation animlr,animtb,animrl,animbt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        tv1=(TextView)findViewById(R.id.textView);
-//        tv2=(TextView)findViewById(R.id.textView2);
-//        tv3=(TextView)findViewById(R.id.textView3);
-//        tv4=(TextView)findViewById(R.id.textView4);
-
-//        tv1.setTextSize(16);
-//        tv2.setTextSize(16);
-//        tv3.setTextSize(16);
-//        tv4.setTextSize(16);
-    }
-
-    public void proceed(View view)
-    {
-        Intent i1=new Intent(this,identification.class);
-        startActivity(i1);
-    }
-
-}
+        b1=(Button)findViewById(R.id.button1);
+        t1=(TextView)findViewById(R.id.textView);
+        t2=(TextView)findViewById(R.id.textView2);
+        t3=(TextView)findViewById(R.id.textView3);
+        t4=(TextView)findViewById(R.id.textView3);
+        animlr = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.l2r);
+        animtb = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.t2b);
+        animrl = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.r2l);
+        animbt = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.b2t);
+        // Move
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t3.startAnimation(animrl);
+                t1.startAnimation(animlr);
+                t2.startAnimation(animtb);
+                t4.startAnimation(animbt);
+            }
+        });
+}}
