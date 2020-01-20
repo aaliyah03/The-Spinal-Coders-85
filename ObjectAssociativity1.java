bc9e8717f3a691222b805a3f0c52f1ed2edc84f5
Chaitanyasuma <43496656+Chaitanyasuma@users.noreply.github.com>
2020-01-20 20:07:40 +0530

    Create ObjectAssociativity1.java

---
+++ ObjectAssociativity1.java
@@ -0,0 +1,24 @@
+ackage com.example.patronus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button);
+    }
+
+    public void func(View view) {
+        Intent i = new Intent(this, market.class);
+        startActivity(i);
+
+    }
+}
